a6b90d86684558d8d042c00743e95a33bcbca516
7844Pritam <pritamsharma7844@gmail.com>
2025-09-16 20:07:11 +0530

    i have created a beutiful green button

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div className='bg-green-600 text-white px-4 py-2 rounded-xl' >Sign Up</div>
+  )
+}
+
+export default Button
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import logo from '../assets/icons/logo.svg'
-import search from '../assets/icons/search.svg'
+import logo from "../assets/icons/logo.svg";
+import search from "../assets/icons/search.svg";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
@@ -8,8 +9,10 @@ const Navbar = () => {
       <div className="flex justify-between items-center container mx-auto">
         {/* image and text  */}
         <div className="flex items-center p-2">
-          <div><img src={logo} /></div>
-          <div  >Garden</div>
+          <div>
+            <img src={logo} />
+          </div>
+          <div>Garden</div>
         </div>
 
         <div className="flex gap-10">
@@ -20,10 +23,12 @@ const Navbar = () => {
           <a>About</a>
         </div>
 
-        <div className="flex gap-5">
-          <div><img  className=" w-7" src={search}/></div>
+        <div className="flex gap-5 items-center">
+          <div>
+            <img className=" w-7" src={search} />
+          </div>
           <div>Add to cart</div>
-          <div>Sigup</div>
+          <div><Button/></div>
         </div>
       </div>
     </div>
